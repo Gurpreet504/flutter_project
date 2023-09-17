3f5c256d00b0cfcc27486548c30bf09fdcd6353b
Gurpreet <gurpreetkaur09012003@gmail.com>
2023-09-17 15:45:19 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_first_app/view/welcome_page/custom_wiget/my_theme.dart';
+import 'package:my_first_app/view/welcome_page/welcome_page.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -16,20 +17,6 @@ class MyApp extends StatelessWidget {
       theme: MyTheme.lightTheme(context),
       darkTheme: MyTheme.darkTheme(context),
       home: const WelcomePage(),
-    );
-  }
-}
-
-class WelcomePage extends StatelessWidget {
-  const WelcomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("wlc Page"),
-      ),
-      body: const Center(child: Text("welcome to clyear mind")),
     );
   }
 }
--- lib/view/welcome_page/welcome_page.dart
+++ lib/view/welcome_page/welcome_page.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-// import 'package:my_first_app/lib/view/welcome_page/welcome_page.dart';
+import 'package:my_first_app/view/welcome_page/components/background.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
-        // appBar: AppBar(
-        //   title: Text("wlc to my Page"),
-        // ),
-        body: Container(
-            width: double.infinity,
-            height: size.height,
-            child: Stack(
-              children: [],
-            )));
+        body: Background(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Welcome to Clear Mind",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    ));
   }
 }
